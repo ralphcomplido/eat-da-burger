047ff91bdd8ad21c955bf20ce015c15f55bff35c
ralphcomplido <rcomplido.la.usa@gmail.com>
2017-11-25 00:30:26 -0800

    added seeds.sql

---
+++ app/db/seeds.sql
@@ -0,0 +1,4 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple-Stacked Avocado Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cali-Chicken Cheeseburger', true);
